116bb7828aea94d5ddbfada21878849795ee2a32
Francisco Presencia <public@francisco.io>
2020-01-14 21:33:44 +0900

    Normalized package

--- atocha.js
+++ atocha.js
@@ -1,11 +1,13 @@
-const swear = require('swear');
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
+const swear = require("swear");
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
 
 export default (command, buffer = 10) => {
   const maxBuffer = buffer * 1024 * 1024;
-  return swear(exec(command, { maxBuffer }).then(out => {
-    if (out.stderr) throw new Error(out.stderr);
-    return out.stdout.trim();
-  }));
+  return swear(
+    exec(command, { maxBuffer }).then(out => {
+      if (out.stderr) throw new Error(out.stderr);
+      return out.stdout.trim();
+    })
+  );
 };
--- atocha.test.js
+++ atocha.test.js
@@ -1,23 +1,27 @@
-import atocha from './atocha';
+import atocha from "./atocha";
 
-describe('atocha', () => {
-  it('is defined', () => {
+describe("atocha", () => {
+  it("is defined", () => {
     expect(atocha).toBeDefined();
   });
 
-  it('can list the directories', async () => {
-    expect(await atocha('ls')).toContain('node_modules');
+  it("can list the directories", async () => {
+    expect(await atocha("ls")).toContain("node_modules");
   });
 
-  it('uses swear()', async () => {
-    expect(await atocha('ls').split('\n')).toContain('node_modules');
+  it("uses swear()", async () => {
+    expect(await atocha("ls").split("\n")).toContain("node_modules");
   });
 
-  it('can handle fatal errors', async () => {
-    await expect(atocha('echho')).rejects.toThrow(/echho: not found/);
+  it("can handle fatal errors", async () => {
+    await expect(atocha("echho")).rejects.toThrow(
+      /(echho: not found|command not found)/
+    );
   });
 
-  it('can handle error messages', async () => {
-    await expect(atocha('>&2 echo "custom error"')).rejects.toThrow(/custom error/);
+  it("can handle error messages", async () => {
+    await expect(atocha('>&2 echo "custom error"')).rejects.toThrow(
+      /custom error/
+    );
   });
 });
