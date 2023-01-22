41a2cb4b9379322af7bccbef92bebacbb162ddaf
Francisco <public@francisco.io>
2023-01-22 22:00:32 +0100

    Upgraded to ESM

--- atocha.js
+++ atocha.js
@@ -1,11 +1,13 @@
-const swear = require("swear");
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
+import swear from "swear";
+import { promisify } from "util";
+import { exec } from "child_process";
+
+const newExec = promisify(exec);
 
 export default (command, buffer = 10) => {
   const maxBuffer = buffer * 1024 * 1024;
   return swear(
-    exec(command, { maxBuffer }).then(out => {
+    newExec(command, { maxBuffer }).then((out) => {
       if (out.stderr) throw new Error(out.stderr);
       return out.stdout.trim();
     })
